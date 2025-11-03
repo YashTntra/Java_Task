a0fbc660bcac00221eb74e508ede0d2b16ade992
YashTntra <yash.thakur@tntra.io>
2025-11-03 17:21:19 +0530

    Completed the get part

---
+++ src/main/java/com/docManagement/system/Controller/DocumentController.java
@@ -0,0 +1,57 @@
+package com.docManagement.system.Controller;
+
+import com.docManagement.system.model.Document;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import com.docManagement.system.service.DocumentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/documents")
+public class DocumentController {
+
+    private final DocumentService service;
+
+    public DocumentController(DocumentService service) {
+        this.service = service;
+    }
+
+
+    @GetMapping
+    public List<Document> list() {
+        return service.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Document> get(@PathVariable Long id) {
+        Document doc = service.getById(id);
+        return ResponseEntity.ok(doc);
+    }
+
+
+
+    // Total number of documents
+    @GetMapping("/total")
+    public long getTotalDocuments() {
+        return service.totalDocuments();
+    }
+
+    // Category-wise count
+    @GetMapping("/category-count")
+    public List<Map<String, Object>> getCategoryWiseCount() {
+        return service.categoryWiseCount();
+    }
+
+}
